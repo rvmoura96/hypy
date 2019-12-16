76712d366d8aa2a037d2e58016d7c4697ec4976e
Renan Moura <renan.demoura96@dieboldnixdorf.com.br>
2019-12-16 10:38:14 -0300

    Update get vm attribute scripts, fixing script on code.

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -18,8 +18,7 @@ current_cache = """[
             "TotalMinutes": 8712.749183333333,
             "TotalSeconds": 522764.951
         },
-        "ParentSnapshotName": null,
-        
+        "ParentSnapshotName": null
     },
     {
         "Name": "vm 03",
